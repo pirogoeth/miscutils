5c0989ab0af5547a21ca2d387e56a0f0dbdb81db
Sean Johnson <me@seanj.dev>
2023-01-28 13:21:42 -0600

    previous work

--- ci/tasks/rust/test.sh
+++ ci/tasks/rust/test.sh
@@ -7,6 +7,11 @@ rustup show active-toolchain
 echo "============"
 echo
 
+if [[ ! -z "${INSTALL_PACKAGES}" ]]
+then
+    apk add --no-cache ${INSTALL_PACKAGES}
+fi
+
 retdir=$(pwd)
 cd ./source
 cargo test
